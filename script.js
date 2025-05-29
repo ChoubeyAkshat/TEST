b9ef782cc70c520486884b409374d0356bdbe14b
ChoubeyAkshat <95644007+ChoubeyAkshat@users.noreply.github.com>
2025-05-30 02:44:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.getElementById("emailForm").addEventListener("submit", function (e) {
-  e.preventDefault();
   const email = this.querySelector("input").value;
   alert(`Thanks for subscribing! We’ll keep you posted: ${email}`);
-  this.reset();
 });
